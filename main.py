13c9db4f999b29e4d44715dcc033559169235da4
npenchev88 <npenchev88@gmail.com>
2025-09-08 11:06:26 +0300

    latest updates

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
+import numpy as np
 import pygad
 
 import pandas as pd
 from knapsack_solvers.utils.timeout import with_timeout
 from knapsack_solvers.algorithms.genetic_algorithm import GeneticAlgorithm
 from knapsack_solvers.algorithms.memetic_algorithm import MemeticAlgorithm
-from knapsack_solvers.algorithms.dynamic_programming import DP
 from knapsack_solvers.algorithms.memetic_algorithm_hybrid_local_search import MemeticAlgorithmHybridLocalSearch
 from knapsack_solvers.algorithms.memetic_algorithm_parameter_tuning import MemeticAlgorithmParameterTuning
 from knapsack_solvers.algorithms.evolution_strategy import EvolutionStrategy
@@ -18,6 +18,7 @@ from knapsack_solvers.data.knapsack_inputs import adjusted_values
 # ITEMS_FOR_EXPLORATION = [3,5,10,15,20,30,50,100,200,300,400,500,600,700,800,900,1000]
 ITEMS_FOR_EXPLORATION = [10, 100, 1000, 10000]
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+INDEPENDENT_RUNS = 30
 
 #
 # Knapsack problem parameters
@@ -31,17 +32,19 @@ data = {
     'Algorithm': [],
     'Sample': [],
     'Result': [],
-    'Time': []
+    'Time': [],
+    'Seed': [],
 }
 
 
-def fill_data(data, result, count, timeout):
+def fill_data(data, result, count, timeout, seed):
     if isinstance(result, str):
         cname = result
         data['Algorithm'].append(cname)
         data['Sample'].append(count)
         data['Result'].append("N/A")
         data['Time'].append(f"> {timeout} seconds")
+        data['Seed'].append(seed)
         return data
     else:
         cname, best_result, time_result = result
@@ -49,6 +52,7 @@ def fill_data(data, result, count, timeout):
         data['Sample'].append(count)
         data['Result'].append(best_result)
         data['Time'].append(time_result)
+        data['Seed'].append(seed)
         return data
 
 
@@ -66,33 +70,33 @@ for number_of_items in ITEMS_FOR_EXPLORATION:
     values_cut = adjusted_values[:number_of_items]
     max_weight = int(sum(weights_cut) / 2)
 
-    dp_solver = DP(weights_cut, values_cut, max_weight)
-    dp_result = with_timeout(timeout, dp_solver.solve, "DYNAMIC PROGRAMMING")
-    fill_data(data, dp_result, number_of_items, timeout)
+    for seed in range(INDEPENDENT_RUNS):
+        np.random.seed(seed)
+        print(f"Running with seed {seed}")
 
-    base_solver = Base(weights_cut, values_cut, max_weight, 100)
-    base_result = base_solver.solve(timeout=timeout)
-    fill_data(data, base_result, number_of_items, timeout)
+        base_solver = Base(weights_cut, values_cut, max_weight, 100)
+        base_result = base_solver.solve(timeout=timeout)
+        fill_data(data, base_result, number_of_items, timeout, seed)
 
-    ga_solver = GeneticAlgorithm(weights_cut, values_cut, max_weight)
-    ga_result = ga_solver.solve(timeout=timeout)
-    fill_data(data, ga_result, number_of_items, timeout)
+        ga_solver = GeneticAlgorithm(weights_cut, values_cut, max_weight)
+        ga_result = ga_solver.solve(timeout=timeout)
+        fill_data(data, ga_result, number_of_items, timeout, seed)
 
-    ma_solver = MemeticAlgorithm(weights_cut, values_cut, max_weight)
-    ma_result = ma_solver.solve(timeout=timeout)
-    fill_data(data, ma_result, number_of_items, timeout)
+        ma_solver = MemeticAlgorithm(weights_cut, values_cut, max_weight)
+        ma_result = ma_solver.solve(timeout=timeout)
+        fill_data(data, ma_result, number_of_items, timeout, seed)
 
-    mahls_solver = MemeticAlgorithmHybridLocalSearch(weights_cut, values_cut, max_weight)
-    mahls_result = mahls_solver.solve(timeout=timeout)
-    fill_data(data, mahls_result, number_of_items, timeout)
+        mahls_solver = MemeticAlgorithmHybridLocalSearch(weights_cut, values_cut, max_weight)
+        mahls_result = mahls_solver.solve(timeout=timeout)
+        fill_data(data, mahls_result, number_of_items, timeout, seed)
 
-    es_solver = EvolutionStrategy(weights_cut, values_cut, max_weight)
-    es_result = es_solver.solve(timeout=timeout)
-    fill_data(data, es_result, number_of_items, timeout)
+        es_solver = EvolutionStrategy(weights_cut, values_cut, max_weight)
+        es_result = es_solver.solve(timeout=timeout)
+        fill_data(data, es_result, number_of_items, timeout, seed)
 
-    de_solver = DifferentialEvolution(weights_cut, values_cut, max_weight)
-    de_result = de_solver.solve(timeout=timeout)
-    fill_data(data, de_result, number_of_items, timeout)
+        de_solver = DifferentialEvolution(weights_cut, values_cut, max_weight)
+        de_result = de_solver.solve(timeout=timeout)
+        fill_data(data, de_result, number_of_items, timeout, seed)
 
 df = pd.DataFrame(data)
 
